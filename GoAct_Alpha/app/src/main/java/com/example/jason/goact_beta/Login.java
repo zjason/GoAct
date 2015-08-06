34eccc7567f57b413dbf9e69b6114c048983dbdb
zjason <jason.zhao1992@gmail.com>
2015-08-06 12:16:36 -0400

    some change
    ….

--- GoAct_Alpha/app/src/main/java/com/example/jason/goact_beta/Login.java
+++ GoAct_Alpha/app/src/main/java/com/example/jason/goact_beta/Login.java
@@ -1,12 +1,13 @@
 package com.example.jason.goact_beta;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Login extends ActionBarActivity {
+public class Login extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
