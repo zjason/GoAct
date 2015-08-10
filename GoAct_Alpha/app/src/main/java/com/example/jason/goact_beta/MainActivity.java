9040eff149f9a459caf203c5864a1a7074e2bba0
zjason <jason.zhao1992@gmail.com>
2015-08-09 21:46:03 -0400

    update 2
    Change image button to menu, fix fragment listener. Change between
    fragment no longer crash.
    
    The icon in fragment feels little bit small. When I change it to no
    text image, it will crash.

--- GoAct_Alpha/app/src/main/java/com/example/jason/goact_beta/MainActivity.java
+++ GoAct_Alpha/app/src/main/java/com/example/jason/goact_beta/MainActivity.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.jason.goact_beta.Fragments.AddEvent;
 import com.example.jason.goact_beta.Fragments.Home;
@@ -17,14 +19,16 @@ import com.example.jason.goact_beta.Fragments.Setting;
 import com.example.jason.goact_beta.Fragments.User;
 
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements Home.OnFragmentInteractionListener, Mapfrag.OnFragmentInteractionListener,AddEvent.OnFragmentInteractionListener,
+       User.OnFragmentInteractionListener, Setting.OnFragmentInteractionListener{
 
     private static final String TAG_HOME = "Home";
-    private static final String TAG_MAP = "Map";
+    private static final String TAG_MAP = "Mapfrag";
     private static final String TAG_ADDEVENT = "AddEvent";
     private static final String TAG_USER= "User";
     private static final String TAG_SETTING = "Setting";
-    private ImageButton bhome,bmap,baddevent,bsetting,buser;
+
+    private int itemId = 0;
     private Fragment home,map,addevent,user,setting;
     private FragmentManager fm;
 
@@ -35,19 +39,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         fm = this.getFragmentManager();
 
-        bhome = (ImageButton) findViewById(R.id.home);
-        bmap = (ImageButton) findViewById(R.id.map);
-        baddevent = (ImageButton) findViewById(R.id.post);
-        bsetting = (ImageButton) findViewById(R.id.settting);
-        buser = (ImageButton) findViewById(R.id.user);
 
-        //bhome.setOnClickListener(this);
-        //bmap.setOnClickListener(this);
-        //baddevent.setOnClickListener(this);
-        //bsetting.setOnClickListener(this);
-        //buser.setOnClickListener(this);
 
-        initHomeFragment();
+        //initHomeFragment();
     }
 
     @Override
@@ -58,63 +52,15 @@ public class MainActivity extends Activity implements View.OnClickListener{
         //finish();
     }
 
-    private void initHomeFragment(){
-
-        Log.d("fragtest", "1");
-        if(fm.findFragmentByTag(TAG_HOME) == null){
-            Log.d("fragtest", "2");
-            home = Home.newInstance("a", "n");
-            Log.d("fragtest", "3");
-            fm.beginTransaction().add(R.id.frag, home, TAG_HOME).commit();
-            Log.d("fragtest", "4");
-        }
-        Log.d("fragtest","5");
-    }
-
     @Override
-    public void onClick(View v){
-        if(v == bhome){
-            removeFragments();
-            home = fm.findFragmentByTag(TAG_HOME);
-            if(home == null){
-                home = Home.newInstance("a","n");
-                fm.beginTransaction().add(R.id.frag,home,TAG_HOME).commit();
-            }
-        }
-        if(v == bmap){
-            removeFragments();
-            map = fm.findFragmentByTag(TAG_MAP);
-            if(map == null){
-                map = Mapfrag.newInstance("a", "n");
-                fm.beginTransaction().add(R.id.frag,map,TAG_MAP).commit();
-            }
-        }
-        if(v == baddevent){
-            removeFragments();
-            addevent = fm.findFragmentByTag(TAG_ADDEVENT);
-            if(addevent == null){
-                addevent = AddEvent.newInstance("a", "n");
-                fm.beginTransaction().add(R.id.frag,addevent,TAG_ADDEVENT).commit();
-            }
-        }
-        if(v == buser){
-            removeFragments();
-            user = fm.findFragmentByTag(TAG_USER);
-            if(user == null){
-                user = User.newInstance("a", "n");
-                fm.beginTransaction().add(R.id.frag,user,TAG_USER).commit();
-            }
-        }
-        if(v == bsetting){
-            removeFragments();
-            setting = fm.findFragmentByTag(TAG_SETTING);
-            if(setting == null){
-                setting = Setting.newInstance("a", "n");
-                fm.beginTransaction().add(R.id.frag,setting,TAG_SETTING).commit();
-            }
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions, menu);
 
+        this.getActionBar().setDisplayShowHomeEnabled(false);
+        this.getActionBar().setDisplayShowTitleEnabled(false);
 
+        return super.onCreateOptionsMenu(menu);
     }
 
     //helper method that removes the current fragment
@@ -123,18 +69,86 @@ public class MainActivity extends Activity implements View.OnClickListener{
     private void removeFragments() {
         if (home != null) {
             fm.beginTransaction().remove(home).commit();
+            Log.d("test", "home is null");
         }
         if (map != null){
             fm.beginTransaction().remove(map).commit();
+            Log.d("test", "map is null");
         }
         if (addevent != null) {
             fm.beginTransaction().remove(addevent).commit();
+            Log.d("test", "post is null");
         }
         if (user != null) {
             fm.beginTransaction().remove(user).commit();
+            Log.d("test", "user is null");
         }
         if (setting != null) {
             fm.beginTransaction().remove(setting).commit();
+            Log.d("test", "setting is null");
         }
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item){
+        if(itemId != item.getItemId()){
+            removeFragments();
+            itemId = item.getItemId();
+        }
+        else{
+            return  false;
+        }
+
+        Log.d("test", "1");
+        switch (itemId){
+            case R.id.action_home:
+
+                home = fm.findFragmentByTag(TAG_HOME);
+                if(home == null){
+                    home = Home.newInstance("a","n");
+                    fm.beginTransaction().add(R.id.frag, home, TAG_HOME).commit();
+                    Log.d("test", "home pressed");
+                }
+                break;
+            case R.id.action_map:
+
+                map = fm.findFragmentByTag(TAG_MAP);
+                if(map == null){
+                    map = Mapfrag.newInstance("a", "n");
+                    fm.beginTransaction().add(R.id.frag, map, TAG_MAP).commit();
+                }
+                break;
+            case R.id.action_post:
+
+                addevent = fm.findFragmentByTag(TAG_ADDEVENT);
+                if(addevent == null){
+                    addevent = AddEvent.newInstance("a", "n");
+                    fm.beginTransaction().add(R.id.frag, addevent, TAG_ADDEVENT).commit();
+                }
+                break;
+            case R.id.action_user:
+
+                user = fm.findFragmentByTag(TAG_USER);
+                if(user == null){
+                    user = User.newInstance("a", "n");
+                    fm.beginTransaction().add(R.id.frag, user, TAG_USER).commit();
+                }
+                break;
+            case R.id.action_settings:
+
+                setting = fm.findFragmentByTag(TAG_SETTING);
+                if(setting == null){
+                    setting = Setting.newInstance("a", "n");
+                    fm.beginTransaction().add(R.id.frag, setting, TAG_SETTING).commit();
+                }
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
